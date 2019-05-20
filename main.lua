cc023a587ed533f5d9d15e76d5fd44cef9768bfd
Davide Belfiori <davidebelfiori@gmail.com>
2019-05-20 16:29:31 +0200

    Added solving algorithm

--- main.lua
+++ main.lua
@@ -1,20 +1,46 @@
 math.randomseed(os.time())
 local Maze = require('maze')
 local Stack = require('stack')
+require("maze-solver")
+
 
 function love.load()
-    -- local windowSize = {1920, 1080}
+    --local windowSize = {1920, 1080}
     local windowSize = {800, 800}
     love.window.setMode(windowSize[1], windowSize[2])
+    love.window.setFullscreen(true)
     -- rows = 78
     -- maze = Maze:new(rows, math.ceil(rows * 1.75))
-    maze = Maze:new(104, 104)
+    maze = Maze:new(50, 50)
     width = windowSize[1] / maze.cols
     height = windowSize[2] / maze.rows
     recursiveBacktrack(maze, maze:getCell(1, 1))
     maze.grid[1][1].walls.top = false
     maze.grid[maze.rows][maze.cols].walls.bottom = false
+
+    -- Reset visited value for each cell 
+
+    for i = 1, maze.rows do
+        for j = 1, maze.cols do
+        maze:getCell(i,j).visited = false
+        end
+    end
+
+
+    -- Prepare solving
+
+    start = {y = 1, x = 1}
+    exit = {x = maze.cols, y = maze.rows}
+
+    c = coroutine.create( Solver["doStep"] )
+
+    step_stack = {start}
+    step_index = 1
+    isFinish = false
+
+    resolve = false
 end
+
 
 function recursiveBacktrack(maze, current, stack)
     stack = stack or Stack:new(current)
@@ -32,6 +58,31 @@ function recursiveBacktrack(maze, current, stack)
     recursiveBacktrack(maze, next, stack)
 end
 
+function love.keypressed(k)
+	if k == 'return' then
+		resolve = not resolve
+    end
+    if k == "escape" then
+        love.event.push('quit')
+    end
+end
+
 function love.draw()
+
+    -- Draw Maze
+
     maze:draw(width, height)
+
+    -- Solve
+
+    if resolve and not isFinish and #step_stack > 0 then
+        current = table.remove( step_stack, step_index)
+        _,isFinish,neighbors = coroutine.resume( c, current, exit, maze)
+        for _,n in pairs(neighbors) do
+          table.insert( step_stack, step_index, n )
+        end
+        if #step_stack > 0 then
+            step_index = (step_index % #step_stack) + 1
+        end
+      end
 end
